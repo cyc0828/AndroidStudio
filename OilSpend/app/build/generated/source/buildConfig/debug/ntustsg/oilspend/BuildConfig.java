0da470a921b8791356dc6eb870e0d2778b55c5ce
tp6m35p4 <tp6m35p4@gmail.com>
2016-12-16 00:36:49 +0800

    Add files via upload

---
+++ OilSpend/app/build/generated/source/buildConfig/debug/ntustsg/oilspend/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ntustsg.oilspend;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ntustsg.oilspend";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
